91dc1278bebd6e39ba2ed13dcac6d3103cc72af6
Sid Stamm <stammsl@rose-hulman.edu>
2019-04-05 15:00:57 -0400

    Add homework5.

---
+++ homework5/problems.c
@@ -0,0 +1,67 @@
+/*
+ * CSSE 132
+ * Rose-Hulman Institute of Technology
+ * Computer Science and Software Engineering
+ *
+ * problems.c: this file is where you implement your solutions
+ *             for the homework assignment.
+ *
+ * This file contains code for homework in the CSSE 132 class.
+ * When you edit this file for class, be sure to put your name here!
+ *
+ * Edited by
+ * NAME: 
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * This function allocates a new string that contains every other character
+ * from the input string.  
+ * For example, skipper("AbAbAb") returns "AAA"
+ *
+ * BE SURE to allocate the right number of bytes, no more and no less.
+ * DO NOT de-allocate or free the string after you make it.
+ *
+ * IMPORTANT: any code using this function MUST de-allocate the string.
+ *
+ * HINT: you may want to copy your "length" function from HW4 into this file so
+ * you can use it.
+ *
+ * @param input - the string to copy some characters from
+ * @return the address of a newly allocated string
+ */
+char* skipper(char* input)
+{
+  // TODO: your code goes here.  You may also want to change the return
+  // statement.
+  return NULL;
+}
+
+
+/**
+ * This function reads a string representation of a positive integer and
+ * returns the value as an int.
+ * For example, string2num("10") returns 10.
+ *
+ * You may assume the input is a properly formed positive integer.  This means
+ * you do not have to handle negative integers or non-integer characters.
+ * you MUST NOT call other c functions in string2num unless you implement them
+ * yourself in this file.
+ *
+ * HINT: you may want to copy your "length" function from HW4 into this file so
+ * you can use it here.
+ *
+ * @param input - the string to interpret as an integer
+ * @return the value as an integer
+ */
+int string2num(char* input)
+{
+  // TODO: your code goes here.  You may also want to change the return
+  // statement.
+  return -1;
+}
+
+/** DO NOT create a main function in this file or the test executable won't work. **/
---
+++ homework5/problems.s
@@ -0,0 +1,91 @@
+/**
+ * CSSE 132
+ * Rose-Hulman Institute of Technology
+ * Computer Science and Software Engineering
+ *
+ * problems.s: this file is where you implement your solutions
+ *             for the homework assignment in ARM assembly.
+ *
+ * NOTE: the .global and .type lines expose the functions to your 
+ * C programs (including the tests).
+ *
+ * This file contains code for homework in the CSSE 132 class.
+ * When you edit this file for class, be sure to put your name here!
+ *
+ * Edited by
+ * NAME:
+ */
+
+
+/**
+ * This function adds two arguments and returns the result.
+ * Remember that the return value is placed in r0 before returning.
+ * 
+ * @param r0: first arg, x
+ * @param r1: second arg, y
+ * @returns the sum of the two
+ */
+.global add
+.type add, %function
+add:
+  /* TODO: finish this procedure here. */
+
+  /* return from this procedure (jump to lr) */
+  bx lr
+
+/**
+ * This function returns the greater of two arguments.
+ * 
+ * You will need to do a comparison for this.  Remember that 
+ * cmp sets all the CSPR registers (EQ, ZERO, etc) and the next
+ * instruction can use them. (BEQ, BLE, etc).
+ * HINT: to copy the value of a register into another, use the "mov"
+ * instruction:
+ *   mov r0, r1 ; copies value of r1 into r0
+ * 
+ * @param r0: first arg, x
+ * @param r1: second arg, y
+ * @returns the greater value of x or y
+ */
+.global greater
+.type greater, %function
+greater:
+  /* TODO: finish this procedure here */
+
+  /* return from this procedure (jump to lr) */
+  bx lr
+
+
+/**
+ * This function sums an array.
+ *
+ * You'll need a loop for this, and will probably want to
+ * use a few registers as temporary storage.
+ *
+ * Some hints:
+ *  * to multiply the value in a register by 2, shift it left one bit.
+ *    This instruction shifts a r4's value left one bit and stores the
+ *    result in r3:
+ *     lsl r3, r4, #1
+ *
+ *  * in ARM, things in [ ] are treated as a memory address. For
+ *    example, the following instruction loads the value in memory at
+ *    the address contained in r4 plus 2 (r3 = Mem[r4+2]):
+ *     ldr r3, [r4, #2]
+ *
+ *  * There are many ways to do this (hard) problem.  If you want to
+ *    try and use fewer instructions, look up ARMv7 addressing modes
+ *    or ask your instructor for tips.
+ * 
+ * @param r0: the base address of the array (pointer)
+ * @param r1: the number of elements in the array
+ * @returns the sum of the elements in the array
+ */
+.global sumArray
+.type sumArray, %function
+sumArray:
+  /* TODO: finish this procedure here */
+
+  /* return from this procedure (jump to lr) */
+  bx lr
+
